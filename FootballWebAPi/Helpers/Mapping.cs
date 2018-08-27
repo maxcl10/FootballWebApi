5b36a1f008fe7b2c75f5398586b3e02477e12d08
maxcl10 <maxime.matter@hotmail.fr>
2018-08-27 09:34:48 +0200

    fix lazy ranking

--- FootballWebAPi/Helpers/Mapping.cs
+++ FootballWebAPi/Helpers/Mapping.cs
@@ -12,16 +12,23 @@ namespace FootballWebSiteApi.Helpers
             { "MULHOUSE BOURTZ C.S", "Mulhouse Bourtz C.S." },
             { "BANTZENHEIM F.C.", "Bantzenheim F.C."},
             { "HUNINGUE A.S", "Huningue A.S."},
-            { "UFFHEIM", "Uffheim F.C."},
+            { "UFFHEIM F.C.", "Uffheim F.C."},
             { "RIEDISHEIM F.C", "Riedisheim F.C."},
-            { "ALTKIRCH", "Altkirch A.S."},
+            { "ALTKIRCH A.S.", "Altkirch A.S."},
             { "HIRSINGUE U.S", "Hirsingue U.S."},
             { "BALLERSDORF EPA", "Ballersdorf EPA"},
             { "MULHOUSE REAL C.F.", "Mulhouse Real C.F."},
             { "KINGERSHEIM F.C", "Kingersheim F.C."},
             { "PFASTATT F.C.", "Pfastatt F.C."},
             { "BARTENHEIM F.C", "Bartenheim F.C."},
-            { "WITTENHEIM U.S. 2", "Wittenheim U.S. 2"}
+            { "WITTENHEIM U.S. 2", "Wittenheim U.S. 2"},
+            { "MULHOUSE A.S.R.S", "Mulhouse A.S.R.S." },
+            { "HABSHEIM F.C.", "Habsheim F.C." },
+            { "HIRTZBACH F.C", "Hirtzbach F.C." },
+            { "BERRWILLER A.S. 2", "Berrwiller A.S. 2" },
+            { "ZILLISHEIM S.S.", "Zillisheim S.S." },
+            { "MONTREUX SPORTS", "Montreux Sports" },
+            { "MULHOUSE R.C.", "Mulhouse R.C." }
         };
 
         public static string GetTeam(string lafaTeamName)
